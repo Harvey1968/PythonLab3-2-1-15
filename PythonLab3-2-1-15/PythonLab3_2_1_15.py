12b739dee1f8a52d60722d4c5dfb0aafa052b842
Ian Harvey <108738675+Harvey1968@users.noreply.github.com>
2022-08-01 11:22:38 +0100

    Update PythonLab3_2_1_15.py

--- PythonLab3-2-1-15/PythonLab3_2_1_15.py
+++ PythonLab3-2-1-15/PythonLab3_2_1_15.py
@@ -1,10 +1,8 @@
 # 3.2.1.15 LAB: Collatz's hypothesis
 #
 # Prompts user for a number (integer)
-# Tranlation "Good day, you wish to play a game, yes?"
-print('"Guten Tag, Sie mochten ein Spiel spielen, ja?"')
-# Tranlation "Please enter a number:"
-c0 = int(input('Bitte geben Sie eine Zahl ein: '))
+print('"Guten Tag, Sie mochten ein Spiel spielen, ja?"')# Tranlation: "Good day, you wish to play a game, yes?"
+c0 = int(input('Bitte geben Sie eine Zahl ein: '))	# Tranlation: "Please enter a number:"
 
 if c0 > 0:
 	steps = 0
@@ -19,10 +17,7 @@ if c0 > 0:
 		print(c0)
 		steps += 1
 	# Outputs how many steps the calculation has taken to complete
-	# Tranlation "This calculation has taken 'steps' steps to complete"
-	print('"Diese Berechnung hat', steps, 'Schritte in Anspruch genommen"\n')
+	print('"Diese Berechnung hat', steps, 'Schritte in Anspruch genommen"\n')	# Tranlation: "This calculation has taken 'steps' steps to complete"
 else:
 	# If the user types an non-negative and non-zero integer number the following error message is displayed:
-	# Tranlation "no, bad number"
-	print('"Nein, schlechte Nummer"\n')
-	 
+	print('"Nein, schlechte Nummer"\n')		# Tranlation: "no, bad number"
